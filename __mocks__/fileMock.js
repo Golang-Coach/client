613791582a62dc607da5d2c5b8f1354fcdfad480
Durgaprasad Budhwani <durgaprasad.budhwani@gmail.com>
2017-12-28 09:39:58 +0530

    Fixed failing test

--- __mocks__/fileMock.js
+++ __mocks__/fileMock.js
@@ -1,4 +1,4 @@
 /**
  * Created by durgaprasad on 10/10/17.
  */
-module.exports = {};
+module.exports = 'test-file-stub';
--- __mocks__/setup.mock.js
+++ __mocks__/setup.mock.js
@@ -2,3 +2,4 @@
  * Created by Durgaprasad Budhwani on 10/6/2017.
  */
 
+jest.mock('github-markdown-css', () => '');
