af567e2d51889c473673457ba5f8ef62400d07a8
Durgaprasad Budhwani <durgaprasad.budhwani@gmail.com>
2018-01-02 07:41:35 +0530

    Removed flow from app-bar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,12 @@
 /**
  ** Created by Durgaprasad Budhwani on 12/18/2017.
  * */
-// @flow
 
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { searchPackages } from './../../actions/search';
-import AppBar from './app-bar';
+import ApplicationBar from './app-bar';
 
 const dispatchToProps = dispatch => bindActionCreators({
   searchPackages,
@@ -18,7 +17,7 @@ type Props = {}
 class Header extends PureComponent<Props> {
   render() {
     return (
-      <AppBar
+      <ApplicationBar
         {...this.props}
       />
     );
