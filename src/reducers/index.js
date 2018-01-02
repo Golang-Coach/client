d578d3eec71ef7e7fbeb149176d83bdde57c4f59
Durgaprasad Budhwani <durgaprasad.budhwani@gmail.com>
2018-01-02 19:07:16 +0530

    1. Added git information in package.json 2. Fixed reducer name

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import operations from './operations';
-import repositories from './repositories-info';
+import repositoriesInfo from './repositories-info';
 import repository from './repository';
 
 const rootReducer = combineReducers({
   operations,
   repository,
-  repositories,
+  repositoriesInfo,
 });
 
 export default rootReducer;
